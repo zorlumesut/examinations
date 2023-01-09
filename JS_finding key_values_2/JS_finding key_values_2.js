2a88199a05657293875771d17e8ba0cc99d22450
zorlumesut <118998719+zorlumesut@users.noreply.github.com>
2023-01-09 13:21:39 +0300

    Add files via upload

---
+++ JS_finding key_values_2/JS_finding key_values_2.js
@@ -0,0 +1,34 @@
+//? nested
+const people = {
+  person1: {
+    name: "Can",
+    surname: "Canan",
+    dob: "1990",
+    job: "developer",
+    salary: "140000",
+    drivingLicense: true,
+  },
+  person2: {
+    name: "John",
+    surname: "Sweet",
+    dob: "1990",
+    job: "tester",
+    salary: "110000",
+    drivingLicense: false,
+  },
+  person3: {
+    name: "Steve",
+    surname: "Job",
+    dob: "2000",
+    job: "developer",
+    salary: "90000",
+    drivingLicense: true,
+  },
+};
+
+for (let person in people) {
+  people[person].job === "developer" &&
+    console.log(
+      `${person} is a developer and his date of birt: ${people[person].dob}`
+    );
+}
